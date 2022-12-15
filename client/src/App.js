ec17ff9165466c14ffb8d5d674c9f4b0c7d4861b
Alexis Torosina <alexistorosina1@gmail.com>
2022-12-15 15:18:25 -0500

    started login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './Login'
 
 function App() {
-  
+    return(
+        <div className='App'>
+            <Login/>
+        </div>
+    )
 }
 
 export default App;
