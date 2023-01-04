2daeb49ad145115ad836f75f9cb916c971f6e790
Alexis Torosina <alexistorosina1@gmail.com>
2023-01-04 17:10:39 -0500

    css

--- client/src/Biceps.js
+++ client/src/Biceps.js
@@ -1,5 +1,4 @@
-import bicepSvg from './Male/Bicep.svg';
-
+import bicepSvg from "./Male/Bicep.svg";
 
 function Biceps({ allExercises }) {
   console.log(allExercises);
@@ -8,20 +7,25 @@ function Biceps({ allExercises }) {
     height: "auto",
   };
 
-  const centerCard ={
+  const centerCard = {
     margin: "auto",
     width: "30%",
     display: "center",
     color: "white",
-  }
+    display: "flex",
+    flexWrap: "wrap",
+    width: "80vw",
+    justifyContent: "center",
+    gap: "40px",
+  };
   return (
-    <div style={centerCard} class="flex flex-col space-y-4 ...">
-        {/* <svg src={bicepSvg} id="biceps"/>  */}
+    <div style={centerCard}>
+      {/* <svg src={bicepSvg} id="biceps"/>  */}
       {allExercises.map((bi) => {
-          return (
-              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        return (
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
             <video class="w-full" controls width="100%" style={vidStyle}>
-              <source src={bi.video} type="video/mp4"/>
+              <source src={bi.video} type="video/mp4" />
             </video>
             <div class="px-6 py-4">
               <div class="font-bold text-xl mb-2">{bi.name}</div>
